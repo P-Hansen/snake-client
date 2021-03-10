18c80ef9ad1b42a913749e162be0da6048a89641
P-hansen <philip.pt.hansen@gmail.com>
2021-03-10 23:37:13 +0000

    arrow keys work!

--- input.js
+++ input.js
@@ -1,11 +1,29 @@
+let connection;
+
 const handleUserInput = (key) => {
   if (key === '\u0003') { //ctrl+c input
     console.log("Goodbye");
     process.exit();
   }
+  console.log(key);
+  switch (key) {
+    case "\033[A"://up
+      connection.write("Move: up");
+      break;
+    case "\033[B"://down
+      connection.write("Move: down");
+      break;
+    case "\033[D"://left
+      connection.write("Move: left");
+      break;
+    case "\033[C"://right
+      connection.write("Move: right");
+      break;
+  };
 };
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -16,4 +34,10 @@ const setupInput = function () {
 
 
 module.exports = setupInput;
-//module.exports = handleUserInput;
+
+/*
+process.stdin.on("data", (key) => {
+
+  }
+});
+*/
--- play.js
+++ play.js
@@ -1,32 +1,9 @@
 const connect = require('./client');
 const setupInput = require('./input');
-//const handleUserInput = require('./input');
 
 console.log('Connecting ...');
 const conn = connect();
 
 conn.write("Name: FIL");
 
-setupInput();
-
-
-
-/*
-process.stdin.on("data", (key) => {
-  console.log(key);
-  switch (key) {
-    case "\033[A"://up
-      console.log("Move: up");
-      break;
-    case "\033[B"://down 
-      console.log("Move: down");
-      break;
-    case "\033[D"://left
-      console.log("Move: left");
-      break;
-    case "\033[C"://right
-      console.log("Move: right");
-      break;
-  }
-});
-*/
+setupInput(conn);
